ee29e8cb626e9d27fea462f84e7ed7b364902e92
huiyaoren <wslshanlin@sina.com>
2016-05-10 22:59:11 +0800

    index.html 轮播图

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -10,32 +10,66 @@ function range(num) {
 }
 
 // 轮播图对象
-function ImgLoop(imgList) {
-    // 图片列表
-    this.imgList = imgList;
-    // 图片数量
-    this.num = imgList.length;
+function ImgLoop(imgList, buttonBox, imgBox, leftArr, rightArr, time) {
+    this.imgList = imgList; // 图片列表
+    this.num = imgList.length; // 图片数量
+    this.buttonBox = buttonBox; // 圆点位置
+    this.imgBox = imgBox;
+    this.time = time; // 图片位置
+
     // 生成圆点
     this.button = function () {
         var frag = document.createDocumentFragment();
         for (var n in range(this.num)) {
             var button = document.createElement("a");
-            button.title = n;
+            button.rel = n; // 未找到更合适属性存放数据 data-*?
+            if (n == 0) {
+                button.style.background = "none";
+            }
             frag.appendChild(button);
         }
         return frag
     };
+    this.createButton = function () {
+        buttonBox.appendChild(this.button())
+    };
+
     // 自动循环
     this.autoRun = function () {
-        //todo
+
+        var buttons = buttonBox.getElementsByTagName("a");
+
+        for (var n in range(this.num)) {
+            if (buttons[n].style.background == "none") {
+                var imgIndex = Number(n);
+            }
+        }
+        setInterval(function () {
+            console.log(imgIndex);
+            if (imgIndex > 3) {
+                imgIndex = 0;
+            }
+            imgBox.src = imgList[imgIndex];
+            for (var i in range(num)) {
+                // todo 覆盖圆点样式
+            }
+            // todo 重新设置圆点样式
+            imgIndex += 1;
+        }, this.time)
     };
+
     // 左侧移入
     this.runLeft = function () {
-        //todo
+        leftArr.onclick = function () {
+            //todo 左侧点击事件
+        }
     };
+
     // 右侧移入
     this.runRight = function () {
-        //todo
+        rightArr.onclick = function () {
+            // todo 右侧点击事件
+        }
     }
 }
 
@@ -45,8 +79,14 @@ var imgList = [
     "static/images/banner2.png",
     "static/images/banner3.png"
 ];
+window.onload = function () {
+    var buttonBox = document.getElementById("banner_box_button");
+    var imgBox = document.getElementById("banner_box_img");
+    var leftArr = document.getElementById("banner_box_left");
+    var rightArr = document.getElementById("banner_box_right");
+    var loop = new ImgLoop(imgList, buttonBox, imgBox, leftArr, rightArr);
+    loop.createButton();
+    loop.autoRun();
+    //buttonBox.appendChild(loop.button())
+};
 
-var loop = new ImgLoop(imgList);
-console.log(loop.button());
-var buttonBox = document.getElementById(".banner_box_button");
-console.log(buttonBox);
