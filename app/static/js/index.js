5bcebced53f1f9389da87f7a5273c6db91dbbbcd
huiyaoren <wslshanlin@sina.com>
2016-05-12 21:56:15 +0800

    index.html / playAccordion() | workLoop()

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,6 +1,13 @@
 /**
  * Created by wslsh on 2016/5/8.
  */
+
+var banner_imgList = [
+    "static/images/banner0.png",
+    "static/images/banner1.png",
+    "static/images/banner2.png",
+    "static/images/banner3.png"
+];
 function range(num) {
     var list = [];
     for (var i = 0; i < num; i++) {
@@ -22,7 +29,6 @@ function ImgLoop(imgList, buttonBox, imgBox, leftArr, rightArr, time) {
         var frag = document.createDocumentFragment();
         for (var n in range(this.num)) {
             var button = document.createElement("a");
-            button.rel = n; // 未找到更合适属性存放数据 data-*?
             if (n == 0) {
                 button.style.background = "none";
             }
@@ -30,6 +36,8 @@ function ImgLoop(imgList, buttonBox, imgBox, leftArr, rightArr, time) {
         }
         return frag
     };
+
+    // 插入圆点
     this.createButton = function () {
         buttonBox.appendChild(this.button())
     };
@@ -44,50 +52,111 @@ function ImgLoop(imgList, buttonBox, imgBox, leftArr, rightArr, time) {
                 var imgIndex = Number(n);
             }
         }
-        setInterval(function () {
+        autoRunLoop = setInterval(function () {
             if (imgIndex > 3) {
                 imgIndex = 0;
             }
-            console.log(imgIndex);
+            //console.log(imgIndex);
 
             imgBox.src = imgList[imgIndex];
-            for (var i in range(this.num)) {
-                // todo 覆盖圆点样式
+            for (var i in range(imgList.length)) {
+                // 覆盖圆点样式
+                buttons[i].style.background = "#747474";
             }
-            // todo 重新设置圆点样式
+            // 重新设置圆点样式
+            buttons[imgIndex].style.background = "none";
+
             imgIndex += 1;
-        }, this.time)
+        }, this.time);
+        //console.log(autoRunLoop);
+        //return autoRunLoop
     };
 
-    // 左侧移入
+    // todo 左侧移入
     this.runLeft = function () {
-        leftArr.onclick = function () {
-            //todo 左侧点击事件
-        }
+        alert(111)
     };
 
-    // 右侧移入
+    // todo 右侧移入
     this.runRight = function () {
-        rightArr.onclick = function () {
-            // todo 右侧点击事件
-        }
+
+    };
+
+
+    //todo 选择圆点切换图片
+    this.selectImg = function () {
+        console.log(autoRunLoop);
+
+        console.log(111); // todo 载入就运行?
+    };
+    // todo 运行
+    this.run = function () {
+        buttonBox.onload = this.createButton();
+        imgBox.onload = this.autoRun();
+        //leftArr.onclick = this.runLeft();
+        //rightArr.onclick = this.runRight();
+        buttonBox.onclick = this.selectImg;
+        //buttonBox.onmouseout = this.autoRun();
+        //imgBox.removeEventListener("load", this.autoRun, true);
+
     }
 }
 
-var imgList = [
-    "static/images/banner0.png",
-    "static/images/banner1.png",
-    "static/images/banner2.png",
-    "static/images/banner3.png"
-];
+// 教师介绍
+function playAccordion(evt) {
+    //console.log(evt.target);
+
+    var accordionBox = document.getElementById("why_box_teacher");
+    var accordions = accordionBox.children;
+
+    if (evt.target.style.width == "50px") {
+        for (var i in range(accordions.length)) {
+            //console.log(accordions[i]);
+
+            accordions[i].style.width = "50px";
+            accordions[i].style.backgroundPosition = "center center";
+        }
+        evt.target.style.width = "auto";
+        evt.target.style.backgroundPosition = "left";
+
+    }
+    return null
+}
+
+// todo 学员作品展示
+function workLoop() {
+    var loopBox = document.getElementById("why_box_work");
+    var loops = loopBox.children;
+    console.log(loops);
+    for(var i in range(loops.length)){
+        loopBox.appendChild(loops[i].cloneNode(true));
+
+    }
+
+
+    var _left = 0;
+
+
+    setInterval(function () {
+        _left -= 10;
+
+
+        for (var i in range(loops.length)) {
+            //console.log(loops[i].offsetLeft);
+            loops[i].style.left = _left+"px";
+        }
+
+    }, 100)
+
+
+}
 window.onload = function () {
     var buttonBox = document.getElementById("banner_box_button");
     var imgBox = document.getElementById("banner_box_img");
     var leftArr = document.getElementById("banner_box_left");
     var rightArr = document.getElementById("banner_box_right");
-    var loop = new ImgLoop(imgList, buttonBox, imgBox, leftArr, rightArr, 2000);
-    loop.createButton();
-    loop.autoRun();
+    var loop = new ImgLoop(banner_imgList, buttonBox, imgBox, leftArr, rightArr, 1000);
+    loop.run();
     //buttonBox.appendChild(loop.button())
 };
 
